e58e72a68874bef5f1e26ef0e240d596629298a2
jorgefuertes <jorge@jorgefuertes.com>
2010-07-23 19:51:54 +0200

    License GPL3 added.

--- qmid.php
+++ qmid.php
@@ -2,12 +2,25 @@
 <?php
 /* 
  * QMID: Queru's mail IMAP dispatcher.
- * Jorge Fuertes AKA Queru - jorge@jorgefuertes.com
- * July 2009.
+ * © Jorge Fuertes AKA Queru - jorge@jorgefuertes.com
+ * © July 2009.
  * GPL3 Lisenced.
  * @author Jorge Fuertes
  * @version 1.0
- * @package clasifica-correo-imap
+ * @package qmid
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 
